bdba7d888cf4396a0fc66ccd29456f32f452a5cc
Grant <gkinghorn3@icloud.com>
2024-04-15 13:45:41 +0100

    first

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      primaryOrange: "hsl(26, 100%, 55%)",
+      paleOrange: "hsl(25, 100%, 94%)",
+      veryDarkBlue: "hsl(220, 13%, 13%)",
+      darkGrayishBlue: "hsl(219, 9%, 45%)",
+      
+    }
+  },
+  plugins: [],
+}
+
